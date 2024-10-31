f6cf22bcec613294412b887a93ff8cd12708bd12
nandanasheri <nsher3@uic.edu>
2024-10-31 15:55:38 -0500

    unzip extracts _chat.txt and returns blob

--- src/scripts/unzip.ts
+++ src/scripts/unzip.ts
@@ -1,29 +1,31 @@
 import {
     BlobReader,
-    BlobWriter,
-    TextReader,
     TextWriter,
     ZipReader,
-    ZipWriter,
   } from "@zip.js/zip.js";
   
-export async function unzip(zipFileBlob) {
+export async function unzip(zipFileBlob : Blob) {
 
   console.log("GOT HERE")
+  const targetFile = "_chat.txt"
   // Creates a BlobReader object used to read `zipFileBlob`.
   const zipFileReader = new BlobReader(zipFileBlob);
   // Creates a TextWriter object where the content of the first entry in the zip
   // will be written.
-  const helloWorldWriter = new TextWriter();
+  const textWriter = new TextWriter();
   
   // Creates a ZipReader object reading the zip content via `zipFileReader`,
   // retrieves metadata (name, dates, etc.) of the first entry, retrieves its
   // content via `helloWorldWriter`, and closes the reader.
   const zipReader = new ZipReader(zipFileReader);
-  const firstEntry = (await zipReader.getEntries()).shift();
-  const helloWorldText = await firstEntry.getData(helloWorldWriter);
-  await zipReader.close();
+  const allEntries = (await zipReader.getEntries());
+  const chatFile = allEntries.find ((data) => data.filename == targetFile)
   
-  // Displays "Hello world!".
-  console.log(helloWorldText);
+  const chatData = await chatFile.getData(textWriter);
+  await zipReader.close();
+  const chatblob = new Blob([chatData], {
+    type: 'text/plain'
+  });
+ 
+  return chatblob
 }  
