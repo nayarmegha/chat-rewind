16e58708f2a6ce8ab81d808023eca6b2a018cc01
nandanasheri <nsher3@uic.edu>
2024-11-05 12:04:49 -0600

    working on script function

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -1,31 +1,55 @@
 import styles from './StatCard.module.css'
 import retro from '../../assets/retrostripes.svg'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-interface StatCardProps {
-  desc : string, 
-  number : string, 
-  stat : string
+interface jsonData {
+  name : string,
+  date : Date,
+  content : {
+    attach : string,
+    text : string,
+    event : string,
+    react : number
+  }
 }
 
-const StatCard:React.FC<StatCardProps> = ({desc, number, stat}) => {
-  
+const StatCard:React.FC = () => {
+
+  const [jsonData, setJsonData] = useState<[jsonData] | null>(null)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [texts, setTexts] = useState<number>(0)
+
+  const totalMessagesThisYear = () => {
+    if (!isLoaded) {
+      return 0
+    }
+    return console.log(jsonData.length)
+  }
+
   useEffect (() => {
-    const jsonblob = window.sessionStorage.getItem('message_json')
-    
+    const blob = window.sessionStorage.getItem('message_json')
+    const jsonblob = JSON.parse(blob)
+    // console.log(jsonblob)
+    // console.log(jsonblob && !isLoaded)
+    if (jsonblob && !isLoaded) {
+      setJsonData(jsonblob)
+      setIsLoaded(true)
+      totalMessagesThisYear()
+    }
   }, [])
 
   return (
     <div className={styles['card']}>
       <div className={styles['card-left']}>
-        <h3 className={styles['desc']}>{desc}</h3>
-        <h1 className={styles['number']}>{number}</h1>
-        <h1 className={styles['stat']}>{stat}</h1>
+        <h3 className={styles['desc']}>you sent</h3>
+        <h1 className={styles['number']}>{texts}</h1>
+        <h1 className={styles['stat']}>messages this year</h1>
       </div>
       <div className={styles['card-right']}>
         <img src={retro.src} alt='retro'/>
       </div>
     </div>
+   
   )
 }
 
