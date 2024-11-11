e0ebcea3df18cc98b58341fa76d78951e22c0044
nandanasheri <nsher3@uic.edu>
2024-11-11 10:17:55 -0600

    added no text days logic

---
+++ src/components/NoTextDays/NoTextDays.tsx
@@ -0,0 +1,62 @@
+import styles from './NoTextDays.module.css'
+import retro from '../../assets/retrostripes.svg'
+import { useEffect, useState } from 'react'
+
+const NoTextDays = () => {
+
+  const [notextDays, setNoTextDays] = useState<number>(0)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+
+  const totalMessagesThisYear = (jsonData) => {
+    const endDate = new Date()
+    let count = 0
+    let daysTalked = []
+    // script to find number of messages sent over a past year
+    for (const key in jsonData) {
+      const date = new Date(jsonData[key]['date'])
+      const diffInDays = Math.round((endDate.getTime() - date.getTime()) / (1000*60*60*24));
+      if (diffInDays <= 365) {
+        const month   = date.getUTCMonth() + 1 // months from 1-12
+        const day     = date.getUTCDate()
+        const year    = date.getUTCFullYear()
+        // Using padded values, so that 2023/1/7 becomes 2023/01/07
+        const pMonth        = month.toString().padStart(2,"0")
+        const pDay          = day.toString().padStart(2,"0")
+        const formatDate = `${year}/${pMonth}/${pDay}`
+        if (!daysTalked.includes(formatDate)) {
+          daysTalked.push(formatDate)
+        }
+      }
+    }
+
+    count = 365 - daysTalked.length
+    setNoTextDays(count)
+    setIsLoaded(true)
+  }
+
+  useEffect (() => {
+    const blob = window.sessionStorage.getItem('message_json')
+    const jsonblob = JSON.parse(blob)
+
+    totalMessagesThisYear(jsonblob)
+  }, [])
+
+  if (!isLoaded) {
+    return <div></div>
+  }
+
+  return (
+    <div className={styles['card']}>
+      <div className={styles['card-left']}>
+        <h3 className={styles['desc']}>you didn't text each other for</h3>
+        <h1 className={styles['number']}>{notextDays}</h1>
+        <h1 className={styles['stat']}>days this past year</h1>
+        {notextDays < 20 ? <h2 className={styles['desc']}>wow, you must really like talking to each other</h2> : <h2 className={styles['desc']}>wow, you hate each other?</h2> }
+      </div>
+    </div>
+   
+  )
+}
+
+export default NoTextDays
+
