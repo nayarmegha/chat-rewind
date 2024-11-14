11ef50a2fce8e511ca3098160a9dc9a8830e12d9
kevin <kevinjcord@gmail.com>
2024-11-13 19:52:50 -0600

    finished converstion to whatsapp-chat-parser

--- src/scripts/textToJson.ts
+++ src/scripts/textToJson.ts
@@ -1,118 +1,76 @@
-import type { Chat } from '../utils/types'
+import type { newChat, oldChat } from '../utils/types'
+import * as whatsapp from 'whatsapp-chat-parser';
 
-function convertToJson(splitData: string[], isAndroid: boolean) {
-    const jsonData: Chat[] = []
-    for (let i = 1; i < splitData.length; i++) {
-        // initializing each JSON
-        const eachjson: Chat = {
-            date: new Date(),
-            name: '',
+function convertFormat(messages: oldChat[]) {
+    let convertedJSON: newChat[] = []
+
+    for (let oldMsg of messages) {
+        
+        // ignore system message
+        if(oldMsg.author == null)
+            continue
+
+        // basic conversion
+        const newMsg: newChat = {
+            date: oldMsg.date,
+            name: oldMsg.author,
             content: {
-                text: '',
-                attach: '',
-                event: '',
-                reacts: 0,
+                text: oldMsg.message,
+                attach: oldMsg?.attachment?.fileName,
             },
         }
 
-        let text = splitData[i]
-        if (!isAndroid) {
-            const datestring = text
-                .substring(1, text.indexOf(']'))
-                .replace(',', '')
-            const simpleDate = new Date(datestring)
-
-            text = text.substring(text.indexOf(']') + 2)
-            const sender = text.substring(0, text.indexOf(':'))
-            text = text.substring(text.indexOf(':') + 2)
-
-            // Check for not video or voice call
-            if (
-                text.indexOf('Missed voice') != -1 ||
-                text.indexOf('Missed group') != -1 ||
-                text.indexOf('Missed video') != -1
-            ) {
-                eachjson['content']['event'] = text
-            } // Check for attachments
-            else if (text.indexOf('<attached:') != -1) {
-                const end = text.indexOf('>')
-                eachjson['content']['attach'] = text.substring(
-                    text.indexOf('<attached:') + 11,
-                    end
-                )
-            } // Last case : simple text
-            else {
-                eachjson['content']['text'] = text
-            }
-
-            eachjson['date'] = simpleDate
-            eachjson['name'] = sender
-        } else {
-            const datestring = text
-                .substring(0, text.indexOf('-') - 1)
-                .replace(',', '')
-            const simpleDate = new Date(datestring)
-            text = text.substring(text.indexOf('-') + 2)
-            const sender = text.substring(0, text.indexOf(':'))
-            text = text.substring(text.indexOf(':') + 2)
-
-            // Check for not video or voice call
-            if (
-                text.indexOf('Missed voice') != -1 ||
-                text.indexOf('Missed video call') != -1
-            ) {
-                eachjson['content']['event'] = text
-            } // Check for attachments
-            else if (text.indexOf('(file attached)') != -1) {
-                const end = text.indexOf('(file attached)')
-                eachjson['content']['attach'] = text.substring(0, end - 1)
-            } // Last case : simple text
-            else {
-                eachjson['content']['text'] = text
-            }
-
-            eachjson['date'] = simpleDate
-            eachjson['name'] = sender
+        // Check for missed video/voice calls
+        if (oldMsg.message.includes('\u200EMissed voice') || 
+            oldMsg.message.includes('\u200EMissed group') || 
+            oldMsg.message.includes('\u200EMissed video') ||
+            oldMsg.message.includes('\u200EMissed video call')) {       
+                newMsg.content.text = ''
+                newMsg.content.event = oldMsg.message
         }
-
-        jsonData.push(eachjson)
+    
+        convertedJSON.push(newMsg)
     }
-    return jsonData
+
+    return convertedJSON
 }
 
-export async function textToJson(fileblob: Blob) {
-    let jsonblob = new Blob([])
+export async function textToJson(textBlob: Blob) {
 
-    try {
-        const data = await fileblob.text()
-        // removes [U+200E] and [U+202F] characters - default on whatsapp
-        let otherdata = data.replace(/\u200E/g, '')
-        otherdata = otherdata.replace(/\u202F/g, ' ')
-        /* 
-    Using RegEx to pattern match and split for :
-    [mm/dd/yy, hh:mm:ss am/pm] <text>
-    Data can either be of the format [10/16/21, 1:16:36 AM] or 26 Sep 2014 14:43
-    */
-        let jsonData: Chat[] = []
+    // Convert from text blob to json object with whatsapp-chat-parser
+    const data = await textBlob.text()
+    const messages = whatsapp.parseString(data, {
+        parseAttachments: true
+    });
 
-        let splitfile = otherdata.match(
-            /\[\d{1,2}\/\d{1,2}\/\d{2,4}, \d{1,2}:\d{2}:\d{2}\s[AP]+M\] [^[]+/g
-        )
+    //  Convert Messages from this
+    // {
+    //     date: new Date(),
+    //     author: '',
+    //     message: '',
+    //     attachment: {
+    //         fileName: '',
+    //     },
+    // },
 
-        if (splitfile == null) {
-            splitfile = otherdata.match(
-                /\d{1,2}\/\d{1,2}\/\d{2,4}, \d{1,2}:\d{1,2} [AP]+M -[^0-9]+/gi
-            )
-            jsonData = convertToJson(splitfile, true)
-        } else {
-            jsonData = convertToJson(splitfile, false)
-        }
+    // To this...
+    // {
+    //     date: new Date(),
+    //     name: '',
+    //     content: {
+    //         text: '',
+    //         attach: '',
+    //         event: '',
+    //         reacts: 0,
+    //     },
+    // }
 
-        jsonblob = new Blob([JSON.stringify(jsonData)], {
-            type: 'application/json',
-        })
-    } catch (error) {
-        console.log('error reading blob', error)
-    } 
-    return jsonblob
+    const convertedMessages = convertFormat(messages);
+
+    // Convert back into a blob for parsing
+    const jsonBlob = new Blob([JSON.stringify(convertedMessages)], {
+        type: 'application/json',
+    })
+
+    return jsonBlob
 }
